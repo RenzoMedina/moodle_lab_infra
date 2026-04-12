b0d9ff4b0d9cfbaae912d21b1a85e9988e889a71
RenzoMedina <medinast30@gmail.com>
2026-04-11 21:09:48 -0400

    Modified file cloud-init and deleted file install-moodle.sh

--- install-moodle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo ">>> Verified PHP 8.3 is installed"
-until /usr/bin/php8.3 --version > /dev/null 2>&1; do
-    echo ">>> Waiting for PHP 8.3 to be installed..."
-    sleep 5
-done
-echo ">>> PHP 8.3 is installed"
